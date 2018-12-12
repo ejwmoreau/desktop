912651547b177792a87afac1aef26ac821566fd0
Elie Moreau <emoreau@atlassian.com>
2018-12-12 11:51:53 +1100

    When a screen is removed, it's tags should be moved to another screen that does exist

--- home/config/awesome/rc.lua
+++ home/config/awesome/rc.lua
@@ -813,6 +813,25 @@ for i = 1, 9 do
 end
 
 
+-- When a screen is disconnected, the tags on that screen will be moved to
+-- another similar screen, while keeping the same tag focused on that screen.
+tag.connect_signal("request::screen", function(t)
+    for s in screen do
+        if s ~= t.screen then
+            current_tags = s.selected_tags
+
+            move_tag_to_screen(t, s.index)
+
+            awful.tag.viewnone(s)
+            awful.tag.viewmore(current_tags)
+            force_focus(s)
+
+            return
+        end
+    end
+end);
+
+
 clientbuttons = awful.util.table.join(
     awful.button({ }, 1, function (c)
         client.focus = c;
