7a105a81918473ae6cda1d30d9fbb7e8c236d734
ejwmoreau <moreau.ejw@gmail.com>
2020-07-04 01:52:02 +1000

    Cleaning up 'common' role and become usage

--- deploy.sh
+++ deploy.sh
@@ -6,22 +6,9 @@ git submodule update --init --recursive
 
 cwd=$(pwd)
 
-function checkfile() {
-    filename=${1}
-    if [[ -e "${filename}" ]]; then
-        if [[ ! -h "${filename}" ]]; then
-            mv "${filename}" "${filename}.backup"
-            echo "${filename} -> ${filename}.backup"
-        else
-            rm "${filename}"
-        fi
-    fi
-}
-
 #===============================================================================
 # Color
 #-------------------------------------------------------------------------------
-checkfile "${HOME}/.config/base16-shell"
 ln -sT\
     "${cwd}/submodules/github.com/chriskempson/base16-shell"\
     "/${HOME}/.config/base16-shell"
@@ -29,7 +16,5 @@ ln -sT\
 
 ## [ Miscellaneous ] ##
 
-checkfile "${HOME}/.Xmodmap"
 ln -sfT "${cwd}/home/xmodmaprc"  "${HOME}/.Xmodmap"
-checkfile "${HOME}/.git-completion.bash"
 ln -sfT "${cwd}/home/git-completion.bash" "${HOME}/.git-completion.bash"
